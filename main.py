51f8f90aa4f3624f16e392fcf4698f8f0a95efe9
Pogrammar <vatsal-lookout@outlook.com>
2023-08-15 21:13:32 +0530

    Added original bg and kid

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ pygame.init()
 
 win = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
-bg = pygame.image.load('assets/bg.jpg')
-floor = pygame.image.load('assets/floor.png')
+bg = pygame.image.load('assets/bg.png').convert_alpha()
+bg_x = 0
+game_speed = 10
 
-man = Player(10, 320, 100, 100)
+
+man = Player(10, 430, 100, 100)
 
 while True:
     win.blit(bg, (0, 0))
-    win.blit(floor, (0, 720))
-    
 
     for event in pygame.event.get(): #allows the user to exit the game
         if event.type == pygame.QUIT:
@@ -21,7 +21,13 @@ while True:
             exit()
     keys = pygame.key.get_pressed()
     if keys[pygame.K_RIGHT] and man.x < 1220 - man.width - man.vel:
-        man.x += man.vel
+        bg_x -= 10
+        game_speed += 0.025
+        win.fill("white")
+        if bg_x <= -1280:
+            bg_x = 0
+        win.blit(bg, (bg_x, 0))
+        win.blit(bg, (bg_x + 1280, 0))
         man.right = True
         man.standing = False
     else:
@@ -29,7 +35,23 @@ while True:
         man.standing = True
         man.walkCount = 0
         
-    
+    if not(man.isJump):
+        if keys[pygame.K_SPACE]:
+            man.isJump = True
+            man.right = False
+            man.left = False
+            man.walkCount = 0
+    else:
+        if man.jumpCount >= -10:
+            neg = 1
+            if man.jumpCount < 0:
+                neg = -1
+            man.y -= (man.jumpCount ** 2) * 0.3 * neg
+            man.jumpCount -= 1
+        else:
+            man.isJump = False
+            man.jumpCount = 10
+
     man.draw(win)
     clock.tick(60)
     pygame.display.update()
--- sprites/player.py
+++ sprites/player.py
@@ -1,28 +1,28 @@
 import pygame
 win = pygame.display.set_mode((1280, 720))
 
-char = pygame.image.load('assets/player/standing.png')
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, x, y, width, height):
-        self.walk = [pygame.image.load('assets/player/1.png'),
-                     pygame.image.load('assets/player/2.png'),
-                     pygame.image.load('assets/player/3.png'),
-                     pygame.image.load('assets/player/4.png'),
-                     pygame.image.load('assets/player/5.png'),
-                     pygame.image.load('assets/player/6.png'),
-                     pygame.image.load('assets/player/7.png'), 
-                     pygame.image.load('assets/player/8.png'), 
-                     pygame.image.load('assets/player/9.png'),
-                     pygame.image.load('assets/player/10.png')
-                     ]
+
+            
         self.x = x
         self.y = y
         self.vel = 5
-
+        self.isJump = False
+        self.jumpCount = 10
         self.right = False
         self.width = width
         self.height = height
+        self.walk = [pygame.transform.scale(pygame.image.load(f'assets/player/running/1.png'), (self.width, self.height)),
+                     pygame.transform.scale(pygame.image.load(f'assets/player/running/2.png'), (self.width, self.height)),
+                     pygame.transform.scale(pygame.image.load(f'assets/player/running/3.png'), (self.width, self.height)),
+                     pygame.transform.scale(pygame.image.load(f'assets/player/running/4.png'), (self.width, self.height)),
+                     pygame.transform.scale(pygame.image.load(f'assets/player/running/5.png'), (self.width, self.height)),
+                     pygame.transform.scale(pygame.image.load(f'assets/player/running/6.png'), (self.width, self.height)),
+                     pygame.transform.scale(pygame.image.load(f'assets/player/running/7.png'), (self.width, self.height)),
+                     pygame.transform.scale(pygame.image.load(f'assets/player/running/8.png'), (self.width, self.height)),]
+        self.char = pygame.transform.scale(pygame.image.load(f'assets/player/standing.png'), (self.width, self.height))
         self.walkCount = 0
         self.standing = True
 
@@ -30,9 +30,11 @@ class Player(pygame.sprite.Sprite):
         if self.walkCount + 1 >= 27:
             self.walkCount = 0
 
-        if not(self.standing):
+        if not self.standing:
             if self.right:
-                win.blit(self.walk[self.walkCount//5], (self.x,self.y))
+                win.blit(self.walk[self.walkCount // 2 % len(self.walk)], (self.x, self.y))
                 self.walkCount += 1
         else:
-            win.blit(char, (self.x, self.y))
+            win.blit(self.char, (self.x, self.y))
+
+        
