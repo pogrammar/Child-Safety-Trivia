0d2cc81f315343fb5638ab53da773f3b2973637e
Pogrammar <vatsal-lookout@outlook.com>
2023-08-18 23:41:49 +0530

    GAME FINISHED

--- sprites/player.py
+++ sprites/player.py
@@ -19,10 +19,28 @@ class Player(pygame.sprite.Sprite):
             pygame.transform.scale(pygame.image.load(f'assets/player/running/7.png'), (self.width, self.height)),
             pygame.transform.scale(pygame.image.load(f'assets/player/running/8.png'), (self.width, self.height))
         ]
+        self.jump = [
+            pygame.transform.scale(pygame.image.load(f'assets/player/jumping/1.png'), (self.width, self.height)),
+            pygame.transform.scale(pygame.image.load(f'assets/player/jumping/2.png'), (self.width, self.height)),
+            pygame.transform.scale(pygame.image.load(f'assets/player/jumping/3.png'), (self.width, self.height)),
+            pygame.transform.scale(pygame.image.load(f'assets/player/jumping/4.png'), (self.width, self.height)),
+            pygame.transform.scale(pygame.image.load(f'assets/player/jumping/5.png'), (self.width, self.height)),
+            pygame.transform.scale(pygame.image.load(f'assets/player/jumping/6.png'), (self.width, self.height)),
+            pygame.transform.scale(pygame.image.load(f'assets/player/jumping/7.png'), (self.width, self.height)),
+            pygame.transform.scale(pygame.image.load(f'assets/player/jumping/8.png'), (self.width, self.height)),
+            pygame.transform.scale(pygame.image.load(f'assets/player/jumping/9.png'), (self.width, self.height)),
+            pygame.transform.scale(pygame.image.load(f'assets/player/jumping/10.png'), (self.width, self.height)),
+            pygame.transform.scale(pygame.image.load(f'assets/player/jumping/11.png'), (self.width, self.height))
+        ]
         self.image = self.run[0]  # the first image of the list is the standing image
           # set x and y of rect to x and y of image
+
+
+        self.isJump = False
         self.right = True
+        self.jumpCount = 10
         self.walkCount = 0
+        self.gravity = 0
         self.hitbox = (self.x + 20, self.y + 11, 45, 70)
 
         
@@ -33,5 +51,7 @@ class Player(pygame.sprite.Sprite):
             screen.blit(self.run[self.walkCount // 2 % len(self.run)], (self.x, self.y))
             self.walkCount += 1
 
-      
-
+        if self.isJump:
+            screen.blit(self.jump[self.walkCount // 3 % len(self.jump)], (self.x, self.y))
+            self.walkCount += 1
+        self.hitbox = (self.x + 20, self.y + 11, 45, 70)
